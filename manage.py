3cd39b01eb604373695b0c9102496f1845c909f8
Your Name <youremail@example.com>
2018-04-16 19:51:04 +0800

    redis,Flask_script和数据库迁移集成

--- manage.py
+++ manage.py
@@ -2,12 +2,17 @@
 
 from flask import Flask
 from  flask_sqlalchemy import SQLAlchemy
+import redis
+from flask_script import Manager
+from flask_migrate import Migrate,MigrateCommand
 
 app = Flask(__name__)
 
 class Config(object):
 
     DEBUG = True
+    REDIS_HOST = '127.0.0.1'
+    REDIE_PORT = 6379
 
 app.config.from_object(Config)
 
@@ -15,14 +20,24 @@ app.config.from_object(Config)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:mysql@127.0.0.1:3306/iHome'
 # mysql禁⽌追踪数据库增删改
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
+# 在迁移时让app和db建⽴关联
+Migrate(app,db)
 
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST,port=Config.REDIE_PORT)
+
+manager = Manager(app)
+# 将迁移脚本添加到脚本管理器
+manager.add_command('db',MigrateCommand)
 
 @app.route('/')
 def index():
+
+    redis_store.set('name','zhs')
     return "index"
 
 if __name__ == "__main__":
-    app.run()
+    manager.run()
 
