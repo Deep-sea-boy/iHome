7b900e10b52313420a40ef0f704c80f8fa8e2a4e
Your Name <youremail@example.com>
2018-04-16 19:35:04 +0800

    立项和mysql配置

--- manage.py
+++ manage.py
@@ -1,13 +1,28 @@
 #coding:utf-8
 
 from flask import Flask
+from  flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+class Config(object):
+
+    DEBUG = True
+
+app.config.from_object(Config)
+
+# mysql连接配置
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:mysql@127.0.0.1:3306/iHome'
+# mysql禁⽌追踪数据库增删改
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
 
 @app.route('/')
 def index():
     return "index"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
 
