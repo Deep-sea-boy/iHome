70c7e5dc0be6fa82ec9af4ce0e8bd109bd65de39
Your Name <youremail@example.com>
2018-04-19 20:06:12 +0800

    发送短信后端逻辑之发送短信验证码完整逻辑实现

--- iHome/api_1_0/verify.py
+++ iHome/api_1_0/verify.py
@@ -1,14 +1,78 @@
 #coding:utf-8
 
+import json
+import random
+import re
+
 from flask import make_response,request,abort,jsonify,current_app
 
 from iHome import redis_store,constants
 from iHome.utils.captcha.captcha import captcha
 from iHome.utils.response_code import RET
+from iHome.utils.sms import CCP
 from . import api
 
-last_uuid = ''
+@api.route('/sms_code',methods=["POST"])
+def send_sms_code():
+    """发送短信验证码
+    1.获取参数:手机号，验证码，uuid
+    2.判断是否缺少参数，并对手机号格式进行校验
+    3.获取服务器存储的验证码
+    4.跟客户端传入的验证码进行对比
+    5.如果对比成功就生成短信验证码
+    6.调用单例类发送短信
+    7.如果发送短信成功，就保存短信验证码到redis数据库
+    8.响应发送短信的结果
+    """
+    # 1.获取参数:手机号，验证码，uuid
+    json_str = request.data
+    json_dict = json.loads(json_str)
+    mobile = json_dict.get('mobile')
+    imageCode_client = json_dict.get('imageCode')
+    uuid = json_dict.get('uuid')
 
+    # 2.判断是否缺少参数，并对手机号格式进行校验
+    if not all([mobile,imageCode_client,uuid]):
+        return jsonify(errno=RET.PARAMERR,errmsg='缺少参数')
+    if not re.match(r'^1[345678][0-9]{9}$',mobile):
+        return jsonify(errno=RET.PARAMERR,errmsg='手机格式错误')
+
+    #3.获取服务器存储的验证码
+    try:
+        imageCode_server = redis_store.get('ImageCode:%s' %uuid)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR,errmsg='查询验证码失败')
+    if not imageCode_server:
+        return jsonify(errno=RET.NODATA, errmsg='状态码不存在')
+
+    #4.跟客户端传入的验证码进行对比
+    if imageCode_client != imageCode_server:
+        return jsonify(errno=RET.PARAMERR, errmsg='验证码输入有误')
+
+    #5.如果对比成功就生成短信验证码
+    sms_code = '%06d'%random.randint(0,999999)
+    current_app.looger.debug(sms_code)
+
+    #6.调用单例类发送短信
+    result = CCP().send_sms_code(mobile,[sms_code,constants.SMS_CODE_REDIS_EXPIRES/60],'1')
+    if result != 1:
+        return jsonify(errno=RET.THIRDERR,errmsg='发送短信验证码失败')
+
+    #7.如果发送短信成功，就保存短信验证码到redis数据库
+    try:
+        redis_store.set('SMS:%s'%mobile,sms_code,constants.SMS_CODE_REDIS_EXPIRES)
+    except Exception as e :
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg='存储短信验证码失败')
+
+    #8.响应发送短信的结果
+    return jsonify(errno=RET.OK, errmsg='发送短信验证码成功')
+
+
+
+
+last_uuid = ''
 @api.route('/image_code',methods=["GET"])
 def get_image_code():
     """提供图片验证码
