108a33f93b9dd54c599b030f3179389a477d60dd
Your Name <youremail@example.com>
2018-04-19 18:31:27 +0800

    日志加载

--- iHome/api_1_0/verify.py
+++ iHome/api_1_0/verify.py
@@ -1,12 +1,11 @@
 #coding:utf-8
 
-from flask import make_response,request,abort,jsonify
+from flask import make_response,request,abort,jsonify,current_app
 
 from iHome import redis_store,constants
 from iHome.utils.captcha.captcha import captcha
 from iHome.utils.response_code import RET
 from . import api
-
 
 last_uuid = ''
 
@@ -29,12 +28,16 @@ def get_image_code():
     #2.⽣成图⽚验证码的名字，⽂本信息，图⽚
     name,text,image = captcha.generate_captcha()
 
+    # logging.debug('验证码：'+text)
+    current_app.logger.debug('验证码：'+text)
     # 3.使用redis数据库缓存图片验证码，uuid作为key
     try:
         if last_uuid:
             redis_store.delete('ImageCode:%s' %last_uuid)
         redis_store.set('ImageCode:%s' %uuid,text,constants.IMAGE_CODE_REDIS_EXPIRES)
     except Exception as e:
+
+        current_app.logger.error(e)
         return jsonify(errno=RET.DBERR,errmsg=u'保存验证码失败')
 
     global last_uuid
