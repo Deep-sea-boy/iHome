042b13a7226088550b88fb81db91e282c943a01a
Your Name <youremail@example.com>
2018-04-21 19:04:50 +0800

    优化注册逻辑，登录即注册

--- iHome/api_1_0/passport.py
+++ iHome/api_1_0/passport.py
@@ -1,12 +1,49 @@
 #coding:utf-8
+#登录注册
 import re
 
-from flask import request,jsonify,current_app
+from flask import request,jsonify,current_app,session
 
 from iHome import redis_store,db
 from iHome.models import User
 from iHome.utils.response_code import RET
 from . import api
+
+@api.route('/sessions',methods=["POST"])
+def login():
+    """登录
+        1.接受登录参数：手机号，密码明文
+        2.判断参数是否为空，并校验手机号格式
+        3.使用手机号查询用户信息
+        4.对比用户的密码
+        5.写入状态保持信息到session
+        6.响应登录结果
+    """
+    json_dict = request.json
+    mobile = json_dict.get('mobile')
+    password = json_dict.get('password')
+
+    if not all([mobile,password]):
+        return jsonify(errno=RET.PARAMERR,errmsg='缺少参数')
+    if not re.match(r'^1[345678][0-9]{9}$',mobile):
+        return jsonify(errno=RET.PARAMERR, errmsg='手机号格式错误')
+
+    try:
+        user = User.query.filter(User.mobile==mobile).first()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg='查询用户数据失败')
+    if not user:
+        return jsonify(errno=RET.NODATA, errmsg='用户名或1密码错误')
+
+    if not user.check_password(password):
+        return jsonify(errno=RET.NODATA, errmsg='用户名或密码错误')
+
+    session['user_id'] = user.id
+    session['name'] = user.name
+    session['mobile'] = user.mobile
+
+    return jsonify(errno=RET.OK, errmsg='登录成功')
 
 
 @api.route('/users',methods=["POST"])
@@ -50,6 +87,10 @@ def register():
     if sms_code_server != sms_code_client:
         return jsonify(errno=RET.PARAMERR, errmsg='短信验证码输入有误')
 
+    # 判断该手机号是否注册
+    if User.query.filter(User.mobile==mobile).first():
+        return jsonify(errno=RET.DATAEXIST, errmsg='该手机号已注册')
+
     #5.如果对比成功，就创建用户模型User对象，并给属性赋值
     user = User()
     user.mobile = mobile
@@ -65,6 +106,12 @@ def register():
         current_app.logger.error(e)
         db.session.rollback()
         return jsonify(errno=RET.DBERR, errmsg='保存用户注册数据失败')
+
+    # 写入状态保持信息到session
+    session['user_id'] = user.id
+    session['name'] = user.name
+    session['mobile'] = user.mobile
+
 
     #7.响应注册结果
     return jsonify(errno=RET.OK, errmsg='注册成功')
