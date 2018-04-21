3a9a063f6adb99f6290198e453d9f7c8fac6faac
Your Name <youremail@example.com>
2018-04-21 22:24:01 +0800

    自定义判断用户是否登录的装饰器

--- iHome/static/js/ihome/profile.js
+++ iHome/static/js/ihome/profile.js
@@ -18,14 +18,69 @@ $(document).ready(function () {
             // 渲染个人信息界面
             $('#user-avatar').attr('src',response.data.avatar_url);
             $('#user-name').val(response.data.name);
+        }else if (response.errno == '4101') {
+            location.href = 'login.html';
         }else{
             alert(response.errmsg);
         }
     });
 
     // TODO: 管理上传用户头像表单的行为
+    $('#form-avatar').submit(function (event) {
+         // 禁用表单自己的提交行为
+        event.preventDefault();
+
+        // 模拟表单的提价行为：方便读取input里面的file数据，不需要自己写代码获取
+        $(this).ajaxSubmit({
+            url:'/api/1.0/users/avatar',
+            type:'post',
+            headers:{'X-CSRFToken':getCookie('csrf_token')},
+            success:function (response) {
+                if (response.errno =='0'){
+                    $('#user-avatar').attr('src',response.data);
+                }else if (response.errno == '4101') {
+                    location.href = 'login.html';
+                }else {
+                    alert(response.errmsg);
+                }
+            }
+        })
+
+    });
 
     // TODO: 管理用户名修改的逻辑
+    $('#form-name').submit(function (event) {
+        event.preventDefault();
+
+        var new_name = $('#user-name').val();
+        if (!new_name) {
+            alert('请输入用户名');
+        }
+
+        var params = {
+            'name':new_name
+        };
+
+        $.ajax({
+            url: '/api/1.0/users/name',
+            type: 'put',
+            data: JSON.stringify(params),
+            contentType: 'application/json',
+            headers: {'X-CSRFToken': getCookie('csrf_token')},
+            success:function (response) {
+                    if (response.errno == '0') {
+                        // 修改成功，提示成功
+                        showSuccessMsg();
+                    } else if (response.errno == '4101') {
+                        location.href = 'login.html';
+                    }else {
+                        alert(response.errmsg);
+                    }
+            }
+        });
+    });
+
+
 
 });
 
