f0fbd23238d7cab872dc7d6b81fdf70ced1c2515
Your Name <youremail@example.com>
2018-04-22 15:48:39 +0800

    实名认证接口之前端逻辑实现

---
+++ iHome/static/js/ihome/auth.js
@@ -0,0 +1,59 @@
+function showSuccessMsg() {
+    $('.popup_con').fadeIn('fast', function() {
+        setTimeout(function(){
+            $('.popup_con').fadeOut('fast',function(){}); 
+        },1000) 
+    });
+}
+
+
+function getCookie(name) {
+    var r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
+    return r ? r[1] : undefined;
+}
+
+$(document).ready(function(){
+    // TODO: 查询用户的实名认证信息
+
+
+    // TODO: 管理实名信息表单的提交行为
+    $('#form-auth').submit(function (event) {
+        event.preventDefault();
+
+        var real_name = $('#real-name').val();
+        var id_card = $('#id-card').val();
+
+        if (!real_name) {
+            alert('请输入真实姓名');
+        }
+        if (!id_card) {
+            alert('请输入真实身份证号码');
+        }
+
+        var params = {
+            'real_name':real_name,
+            'id_card':id_card
+        };
+        $.ajax({
+            url:'/api/1.0/users/auth',
+            type:'post',
+            data:JSON.stringify(params),
+            contentType:'application/json',
+            headers:{'X-CSRFToken':getCookie('csrf_token')},
+            success:function (reponse) {
+                if(reponse.errno=='0'){
+
+                    // 因为实名认证只有一次，所以一旦认证成功，就要将inout设置为不可交互，并且要影藏保存按钮
+                    $('#real-name').attr('disabled', true);
+                    $('#id-card').attr('disabled', true);
+                    $('.btn-success').hide();
+                }else if (response.errno == '4101') {
+                    location.href = 'login.html';
+                } else {
+                    alert(response.errmsg);
+                }
+            }
+        })
+    })
+
+});
